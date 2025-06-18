00139e99c347c6a92b56f7056d9b952a20a5cde7
haroon-ahmed01 <haroonahmed5652@gmail.com>
2025-06-18 14:46:29 +0530

    config cors

--- server/server.js
+++ server/server.js
@@ -10,8 +10,18 @@ const PORT = process.env.PORT || 5000;
 // -------------------- MIDDLEWARE --------------------
 
 // Enable Cross-Origin Resource Sharing
-app.use(cors());
+const allowedOrigins = [
+  process.env.FRONTEND_URL, 
+  // "http://localhost:5173"
+];
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 // Parse JSON request bodies
 app.use(express.json());
 
